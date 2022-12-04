acb74cdcac9d495fdf0ca11056259810d05c99d3
Jayden Dollente <dollentej0107@gmail.com>
2022-12-04 12:21:04 -0800

    Updates

---
+++ precache-manifest.11874c434f93cd16fa918457f955ca69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e262e37ca18482ed8b1",
+    "url": "/twitter-clone/static/css/main.4836fec2.chunk.css"
+  },
+  {
+    "revision": "2e262e37ca18482ed8b1",
+    "url": "/twitter-clone/static/js/main.2e262e37.chunk.js"
+  },
+  {
+    "revision": "d48f06b542086f619008",
+    "url": "/twitter-clone/static/js/1.d48f06b5.chunk.js"
+  },
+  {
+    "revision": "83c0d2e0028f4df90174",
+    "url": "/twitter-clone/static/js/2.83c0d2e0.chunk.js"
+  },
+  {
+    "revision": "017e3e367704848d8507",
+    "url": "/twitter-clone/static/js/runtime~main.017e3e36.js"
+  },
+  {
+    "revision": "5e2dba415c65d4e7d054e5aa608cd808",
+    "url": "/twitter-clone/index.html"
+  }
+];
